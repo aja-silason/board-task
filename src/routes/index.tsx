345500b351692d3c7a7d5d1ffd1c910ee107443a
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-22 17:23:02 +0100

    feat: add drag and drop code without lib

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { LogIn } from "../board";
 import { NotFound } from "../board/NotFound.screen";
 import { Register } from "../board/register";
 import { Home } from "../board/page";
+import DragAndDropApp from "../board/page/drag";
 
 export function RoutesApp() {
     return (
@@ -12,6 +13,7 @@ export function RoutesApp() {
                 <Route path="*" element={<NotFound/>} />
                 <Route path="/" element={<LogIn/>} />
                 <Route path="/home" element={<Home/>} />
+                <Route path="/drag" element={<DragAndDropApp/>} />
                 <Route path="/register" element={<Register/>} />
             </Routes>
 
