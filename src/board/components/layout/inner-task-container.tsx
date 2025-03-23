0b31f572b59ac5b95c3c27e1447d7762fafcf3e2
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-23 17:23:51 +0100

    feat: send thing

--- src/board/components/layout/inner-task-container.tsx
+++ src/board/components/layout/inner-task-container.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react"
-import { NavBar } from "./navbar";
 import { SideBar } from "./sidebar";
 import { Text } from "../text/text";
+import { InnerTaskSideBar } from "./sidebar/inner-sidebarr";
 
 type props = {
     children: ReactNode;
@@ -11,11 +11,10 @@ export const InnerTaskContainer = ({children}: props) => {
     return (
         <div className="flex flex-col">
 
-            <Text text="Tarefa"/>
+            <Text text="Nome da Tarefa"/>
             
             <div className="flex gap-[1em] h-full bg-white">
-                <SideBar/>
-                
+                <InnerTaskSideBar/>
                 <div className="p-[1em] w-full bg-white">
                     {children}
                 </div>
--- src/board/components/layout/sidebar/inner-sidebarr.tsx
+++ src/board/components/layout/sidebar/inner-sidebarr.tsx
@@ -1,18 +1,23 @@
-import { BuildingOffice, Table } from "@phosphor-icons/react"
+import { BuildingOffice, Table, UsersThree } from "@phosphor-icons/react"
 import { ReactNode } from "react"
 import { Link, useLocation } from "react-router-dom"
+import { useToogle } from "../../../hook/behavior/useToogle"
+import { TabButton } from "../../button/tab.button"
 
 type menuProps = {
-    to: string,
+    to: VoidFunction,
+    where: boolean,
     icon: ReactNode,
     menutext: string
 }
-export const SideBar = () => {
+export const InnerTaskSideBar = () => {
+
+    const {isfirst, isSecond, isThird, handleIsFirst, handleIsSecond, handleIsThird} = useToogle();
 
     const menu: menuProps[] = [
-        {to: "/home", icon: <BuildingOffice size={20}/>, menutext: "Início"},
-        {to: "/tasks", icon: <BuildingOffice size={20}/>, menutext: "Tarefas"},
-        {to: "/board", icon: <Table size={20}/>, menutext: "Quadro"},
+        {to: () => handleIsFirst(), where: isfirst, icon: <BuildingOffice size={20}/>, menutext: "Tarefa"},
+        {to: () => handleIsSecond(), where: isSecond, icon: <Table size={20}/>, menutext: "Quadro"},
+        {to: () => handleIsThird(), where: isThird, icon: <UsersThree size={20}/>, menutext: "Participantes"},
     ] 
 
     const location = useLocation();
@@ -23,24 +28,11 @@ export const SideBar = () => {
             {
                 menu?.map((menu: menuProps) => {
 
-                    const isActive = location?.pathname === menu?.to;
+                    const isActive = 0;
 
                     return (
 
-                        <nav>
-                            <Link to={menu?.to}>
-                                <li className={`flex ${isActive ? 'bg-[#000]' : 'bg-white text-[#444]'} text-white p-[.5em] font-[600] rounded-[.3em] gap-[.5em]`}>
-                                    <div className="w-[.2em] rounded-[.5em] bg-[#fff]"></div>
-                                    <div className="flex gap-[.3em] items-center">
-                                        {menu?.icon}
-                                        <p className="">{menu?.menutext}</p>
-                                    </div>
-                                </li>
-                            </Link>
-
-
-                        </nav>
-
+                        <TabButton title={menu?.menutext} onClick={menu?.to} isActive={menu?.where} key={1} />
 
                     )
                 })
@@ -50,3 +42,17 @@ export const SideBar = () => {
     )
 
 }
+
+
+{/*
+    <nav>
+        <li className={`flex ${isActive ? 'bg-[#000]' : 'bg-white text-[#444]'} text-white p-[.5em] font-[600] rounded-[.3em] gap-[.5em]`}>
+            <div className="w-[.2em] rounded-[.5em] bg-[#fff]"></div>
+            <div className="flex gap-[.3em] items-center">
+                {menu?.icon}
+                <p className="">{menu?.menutext}</p>
+            </div>
+        </li>
+
+    </nav>
+*/}
